3fc4a5c14e74bb1bf96914b3bbbf762d45cb86a2
Daniel Serkowski <dserkowski@soldevelo.com>
2017-10-17 15:08:58 +0200

    ATF-1: Fixed problems with requests to the atomfeed admin form

--- omod/src/main/java/org/openmrs/module/atomfeed/web/controller/AtomfeedmoduleController.java
+++ omod/src/main/java/org/openmrs/module/atomfeed/web/controller/AtomfeedmoduleController.java
@@ -9,44 +9,31 @@
  */
 package org.openmrs.module.atomfeed.web.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.User;
-import org.openmrs.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * This class configured as controller using annotation and mapped with the URL of
- * 'module/${rootArtifactid}/${rootArtifactid}Link.form'.
- */
-@Controller("${rootrootArtifactid}.AtomfeedmoduleController")
-@RequestMapping(value = "module/${rootArtifactid}/${rootArtifactid}.form")
+@Controller
 public class AtomfeedmoduleController {
 	
 	/** Logger for this class and subclasses */
 	protected final Log log = LogFactory.getLog(getClass());
 	
-	@Autowired
-	UserService userService;
-	
 	/** Success form view name */
-	private final String VIEW = "/module/${rootArtifactid}/${rootArtifactid}";
+	private final String VIEW = "/module/atomfeed/atomfeed";
 	
 	/**
 	 * Initially called after the getUsers method to get the landing form name
 	 * 
 	 * @return String form view name
 	 */
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "module/atomfeed/atomfeed", method = RequestMethod.GET)
 	public String onGet() {
 		return VIEW;
 	}
@@ -59,7 +46,7 @@ public class AtomfeedmoduleController {
 	 * @param errors
 	 * @return
 	 */
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(value = "module/atomfeed/atomfeed", method = RequestMethod.POST)
 	public String onPost(HttpSession httpSession, @ModelAttribute("anyRequestObject") Object anyRequestObject,
 	        BindingResult errors) {
 		
@@ -68,20 +55,6 @@ public class AtomfeedmoduleController {
 		}
 		
 		return VIEW;
-	}
-	
-	/**
-	 * This class returns the form backing object. This can be a string, a boolean, or a normal java
-	 * pojo. The bean name defined in the ModelAttribute annotation and the type can be just defined
-	 * by the return type of this method
-	 */
-	@ModelAttribute("users")
-	protected List<User> getUsers() throws Exception {
-		List<User> users = userService.getAllUsers();
-		
-		// this object will be made available to the jsp page under the variable name
-		// that is defined in the @ModuleAttribute tag
-		return users;
 	}
 	
 }
