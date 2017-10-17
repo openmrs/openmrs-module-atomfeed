3cdc6aa0c3eac00695f7d14fe58a3d12031b3bda
Daniel Serkowski <dserkowski@soldevelo.com>
2017-10-17 14:11:37 +0200

    ATF-1: OpenMRS module template cleanup

--- api/src/main/java/org/openmrs/module/atomfeed/api/AtomfeedmoduleService.java
+++ api/src/main/java/org/openmrs/module/atomfeed/api/AtomfeedmoduleService.java
@@ -9,40 +9,8 @@
  */
 package org.openmrs.module.atomfeed.api;
 
-import org.openmrs.annotation.Authorized;
-import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
-import org.openmrs.module.atomfeed.AtomfeedmoduleConfig;
-import org.openmrs.module.atomfeed.Item;
-import org.springframework.transaction.annotation.Transactional;
 
-/**
- * The main service of this module, which is exposed for other modules. See
- * moduleApplicationContext.xml on how it is wired up.
- */
 public interface AtomfeedmoduleService extends OpenmrsService {
 	
-	/**
-	 * Returns an item by uuid. It can be called by any authenticated user. It is fetched in read
-	 * only transaction.
-	 * 
-	 * @param uuid
-	 * @return
-	 * @throws APIException
-	 */
-	@Authorized()
-	@Transactional(readOnly = true)
-	Item getItemByUuid(String uuid) throws APIException;
-	
-	/**
-	 * Saves an item. Sets the owner to superuser, if it is not set. It can be called by users with
-	 * this module's privilege. It is executed in a transaction.
-	 * 
-	 * @param item
-	 * @return
-	 * @throws APIException
-	 */
-	@Authorized(AtomfeedmoduleConfig.MODULE_PRIVILEGE)
-	@Transactional
-	Item saveItem(Item item) throws APIException;
 }
--- api/src/main/java/org/openmrs/module/atomfeed/api/impl/AtomfeedmoduleServiceImpl.java
+++ api/src/main/java/org/openmrs/module/atomfeed/api/impl/AtomfeedmoduleServiceImpl.java
@@ -9,44 +9,9 @@
  */
 package org.openmrs.module.atomfeed.api.impl;
 
-import org.openmrs.api.APIException;
-import org.openmrs.api.UserService;
 import org.openmrs.api.impl.BaseOpenmrsService;
-import org.openmrs.module.atomfeed.Item;
 import org.openmrs.module.atomfeed.api.AtomfeedmoduleService;
-import org.openmrs.module.atomfeed.api.dao.AtomfeedmoduleDao;
 
 public class AtomfeedmoduleServiceImpl extends BaseOpenmrsService implements AtomfeedmoduleService {
 	
-	AtomfeedmoduleDao dao;
-	
-	UserService userService;
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setDao(AtomfeedmoduleDao dao) {
-		this.dao = dao;
-	}
-	
-	/**
-	 * Injected in moduleApplicationContext.xml
-	 */
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-	
-	@Override
-	public Item getItemByUuid(String uuid) throws APIException {
-		return dao.getItemByUuid(uuid);
-	}
-	
-	@Override
-	public Item saveItem(Item item) throws APIException {
-		if (item.getOwner() == null) {
-			item.setOwner(userService.getUser(1));
-		}
-		
-		return dao.saveItem(item);
-	}
 }
