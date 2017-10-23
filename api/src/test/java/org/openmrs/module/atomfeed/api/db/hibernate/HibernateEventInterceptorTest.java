3f307d931fd694b9fb25422d8f3325873d2af15e
Daniel Serkowski <dserkowski@soldevelo.com>
2017-10-23 17:52:47 +0200

    ATF-2: Added onSave method tests

--- api/src/test/java/org/openmrs/module/atomfeed/api/db/hibernate/HibernateEventInterceptorTest.java
+++ api/src/test/java/org/openmrs/module/atomfeed/api/db/hibernate/HibernateEventInterceptorTest.java
@@ -3,13 +3,13 @@ package org.openmrs.module.atomfeed.api.db.hibernate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.openmrs.OpenmrsObject;
-import org.openmrs.module.atomfeed.api.db.EventManager;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 import java.util.HashSet;
 import java.util.Stack;
+
+import static org.mockito.Mockito.mock;
 
 public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTest {
 	
@@ -29,7 +29,7 @@ public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTes
 	private ThreadLocal<Stack<HashSet<OpenmrsObject>>> voidedObjects;
 	
 	private ThreadLocal<Stack<HashSet<OpenmrsObject>>> unvoidedObjects;
-
+	
 	private HibernateEventInterceptor hibernateEventInterceptor = new HibernateEventInterceptor();
 	
 	@Before
@@ -40,7 +40,7 @@ public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTes
 	@Test
 	public void afterTransactionBegin_shouldInitStackWithOneEmptyHashSet() {
 		hibernateEventInterceptor.afterTransactionBegin(null);
-
+		
 		Assert.assertEquals(1, inserts.get().size());
 		Assert.assertEquals(1, updates.get().size());
 		Assert.assertEquals(1, deletes.get().size());
@@ -48,7 +48,7 @@ public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTes
 		Assert.assertEquals(1, unretiredObjects.get().size());
 		Assert.assertEquals(1, voidedObjects.get().size());
 		Assert.assertEquals(1, unvoidedObjects.get().size());
-
+		
 		Assert.assertEquals(0, inserts.get().peek().size());
 		Assert.assertEquals(0, updates.get().peek().size());
 		Assert.assertEquals(0, deletes.get().peek().size());
@@ -59,8 +59,21 @@ public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTes
 	}
 	
 	@Test
-	public void onSave() {
-
+	public void onSave_shouldSaveOpenmrsObject() {
+		final OpenmrsObject expected = mock(OpenmrsObject.class);
+		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
+		
+		hibernateEventInterceptor.onSave(expected, null, null, null, null);
+		Assert.assertTrue(inserts.get().peek().contains(expected));
+	}
+	
+	@Test
+	public void onSave_shouldNotSaveNotOpenmrsObject() {
+		final Object notOpenmrsObject = mock(Object.class);
+		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
+		
+		hibernateEventInterceptor.onSave(notOpenmrsObject, null, null, null, null);
+		Assert.assertFalse(inserts.get().peek().contains(notOpenmrsObject));
 	}
 	
 	@Test
