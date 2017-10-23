56250a26dc22959e26456d7854e64883ad93e9b7
Daniel Serkowski <dserkowski@soldevelo.com>
2017-10-23 14:34:36 +0200

    ATF-2: Added HibernateEventInterceptorTest template

---
+++ api/src/test/java/org/openmrs/module/atomfeed/api/db/hibernate/HibernateEventInterceptorTest.java
@@ -0,0 +1,32 @@
+package org.openmrs.module.atomfeed.api.db.hibernate;
+
+import org.junit.Test;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+public class HibernateEventInterceptorTest extends BaseModuleContextSensitiveTest {
+
+    @Test
+    public void afterTransactionBegin() throws Exception {
+    }
+
+    @Test
+    public void onSave() throws Exception {
+    }
+
+    @Test
+    public void onFlushDirty() throws Exception {
+    }
+
+    @Test
+    public void onDelete() throws Exception {
+    }
+
+    @Test
+    public void onCollectionUpdate() throws Exception {
+    }
+
+    @Test
+    public void afterTransactionCompletion() throws Exception {
+    }
+
+}
