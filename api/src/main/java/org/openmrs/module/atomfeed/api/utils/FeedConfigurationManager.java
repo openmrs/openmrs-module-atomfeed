c4566fe4b262777d47dbacb021970964f07101a5
Arkadiusz Lalo <alalo@soldevelo.com>
2017-10-23 10:33:49 +0200

    ATF-6: Fixed after review

--- api/src/main/java/org/openmrs/module/atomfeed/api/utils/FeedConfigurationManager.java
+++ api/src/main/java/org/openmrs/module/atomfeed/api/utils/FeedConfigurationManager.java
@@ -10,49 +10,49 @@ import java.util.HashMap;
 
 @Component
 public class FeedConfigurationManager {
-	
-	private HashMap<String, FeedConfiguration> feedConfiguration;
-	
-	public FeedConfigurationManager() {
-		if (localConfigurationExists()) {
-			loadConfigurations(AtomfeedUtils
-			        .parseFileToJsonConfigurationResource(AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION));
-		} else {
-			loadConfigurations(AtomfeedUtils
-			        .parseFileToJsonConfigurationResource(AtomfeedmoduleConstants.ATOMFEED_PATH_TO_DAFAULT_CONFIGURATION));
-		}
-	}
-	
-	public void loadLocalConfiguration(FeedConfiguration[] value) {
-		AtomfeedUtils.parseJsonConfigurationResourceToFile(value,
-		    AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
-		loadConfigurations(value);
-	}
-	
-	public void loadLocalConfiguration(String value) {
-		if (AtomfeedUtils.isValidateJson(value)) {
-			FeedConfiguration[] localConfiguration = AtomfeedUtils.parseStringToJsonConfigurationResource(value);
-			AtomfeedUtils.parseJsonConfigurationResourceToFile(localConfiguration,
-			    AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
-			loadConfigurations(localConfiguration);
-		}
-	}
-	
-	public FeedConfiguration getConfigurationByTitle(String title) {
-		return feedConfiguration.get(title);
-	}
-	
-	private void loadConfigurations(FeedConfiguration[] feedConfigurations) {
+
+    private HashMap<String, FeedConfiguration> feedConfiguration;
+
+    public FeedConfigurationManager() {
+        if (localConfigurationExists()) {
+            loadConfigurations(AtomfeedUtils
+                    .parseFileToJsonConfigurationResource(AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION));
+        } else {
+            loadConfigurations(AtomfeedUtils
+                    .parseFileToJsonConfigurationResource(AtomfeedmoduleConstants.ATOMFEED_PATH_TO_DAFAULT_CONFIGURATION));
+        }
+    }
+
+    public void loadLocalConfiguration(FeedConfiguration[] value) {
+        AtomfeedUtils.parseJsonConfigurationResourceToFile(value,
+            AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
+        loadConfigurations(value);
+    }
+
+    public void loadLocalConfiguration(String value) {
+        if (AtomfeedUtils.isValidateJson(value)) {
+            FeedConfiguration[] localConfiguration = AtomfeedUtils.parseStringToJsonConfigurationResource(value);
+            AtomfeedUtils.parseJsonConfigurationResourceToFile(localConfiguration,
+                AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
+            loadConfigurations(localConfiguration);
+        }
+    }
+
+    public FeedConfiguration getConfigurationByTitle(String title) {
+        return feedConfiguration.get(title);
+    }
+
+    private void loadConfigurations(FeedConfiguration[] feedConfigurations) {
         HashMap<String, FeedConfiguration> tmp = new HashMap<>();
         for (FeedConfiguration configuration : Arrays.asList(feedConfigurations)) {
             tmp.put(configuration.getTitle(), configuration);
         }
         feedConfiguration = tmp;
     }
-	
-	private boolean localConfigurationExists() {
-		InputStream in = AtomfeedUtils.class.getClassLoader().getResourceAsStream(
-		    AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
-		return in != null;
-	}
+
+    private boolean localConfigurationExists() {
+        InputStream in = AtomfeedUtils.class.getClassLoader().getResourceAsStream(
+            AtomfeedmoduleConstants.ATOMFEED_PATH_TO_LOCAL_CONFIGURATION);
+        return in != null;
+    }
 }
