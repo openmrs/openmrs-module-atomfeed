f5f961b4df68ca9d178b1371746a52325a170c0b
Daniel Serkowski <dserkowski@soldevelo.com>
2017-10-24 11:55:26 +0200

    ATF-2: Added afterTransactionCompletion method tests

--- api/src/test/java/org/openmrs/module/atomfeed/api/db/hibernate/HibernateEventInterceptorTest.java
+++ api/src/test/java/org/openmrs/module/atomfeed/api/db/hibernate/HibernateEventInterceptorTest.java
@@ -1,25 +1,38 @@
 package org.openmrs.module.atomfeed.api.db.hibernate;
 
+import org.hibernate.Transaction;
 import org.hibernate.collection.spi.PersistentCollection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.openmrs.OpenmrsObject;
+import org.openmrs.module.atomfeed.api.db.EventAction;
+import org.openmrs.module.atomfeed.api.db.EventManager;
 
 import java.util.HashSet;
 import java.util.Stack;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class HibernateEventInterceptorTest {
-
+	
 	@Mock
-	private final OpenmrsObject openmrsObject = mock(OpenmrsObject.class);
-
+	private EventManager eventManager;
+	
 	@Mock
-	private final Object notOpenmrsObject = mock(Object.class);
+	private OpenmrsObject openmrsObject;
+	
+	@Mock
+	private Object notOpenmrsObject;
 	
 	private ThreadLocal<Stack<HashSet<OpenmrsObject>>> inserts;
 	
@@ -35,10 +48,12 @@ public class HibernateEventInterceptorTest {
 	
 	private ThreadLocal<Stack<HashSet<OpenmrsObject>>> unvoidedObjects;
 	
+	@InjectMocks
 	private HibernateEventInterceptor hibernateEventInterceptor = new HibernateEventInterceptor();
 	
 	@Before
 	public void setup() {
+		MockitoAnnotations.initMocks(this);
 		initVariables();
 	}
 	
@@ -88,47 +103,107 @@ public class HibernateEventInterceptorTest {
 	@Test
 	public void onDelete_shouldAddOpenmrsObjectToDeleteSet() {
 		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
-
+		
 		hibernateEventInterceptor.onDelete(openmrsObject, null, null, null, null);
 		Assert.assertTrue(deletes.get().peek().contains(openmrsObject));
 	}
-
+	
 	@Test
 	public void onDelete_shouldNotAddNotOpenmrsObjectToDeleteSet() {
 		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
-
+		
 		hibernateEventInterceptor.onSave(notOpenmrsObject, null, null, null, null);
 		Assert.assertFalse(deletes.get().peek().contains(notOpenmrsObject));
 	}
-
+	
 	@Test
 	public void onCollectionUpdate_shouldAddOwnerOfUpdatedCollectionToUpdateSet() {
 		final OpenmrsObject expectedCollectionOwner = openmrsObject;
-
+		
 		final PersistentCollection updatedCollection = mock(PersistentCollection.class);
 		when(updatedCollection.getOwner()).thenReturn(expectedCollectionOwner);
-
+		
 		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
-
+		
 		hibernateEventInterceptor.onCollectionUpdate(updatedCollection, null);
 		Assert.assertTrue(updates.get().peek().contains(expectedCollectionOwner));
 	}
-
+	
 	@Test
 	public void onCollectionUpdate_shouldNotAddOwnerOfUpdatedCollectionWhenTheyAreNotOpenmrsObject() {
 		final Object collectionOwner = notOpenmrsObject;
-
+		
 		final PersistentCollection updatedCollection = mock(PersistentCollection.class);
 		when(updatedCollection.getOwner()).thenReturn(collectionOwner);
-
+		
 		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
-
+		
 		hibernateEventInterceptor.onCollectionUpdate(updatedCollection, null);
 		Assert.assertFalse(updates.get().peek().contains(collectionOwner));
 	}
 	
 	@Test
-	public void afterTransactionCompletion() {
+	public void afterTransactionCompletion_shouldCallServeEventMethodInEventManagerOnEveryObjectInStack() {
+		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
+		
+		addOpenmrsObjectToAllSets();
+		
+		final Transaction transaction = mock(Transaction.class);
+		when(transaction.wasCommitted()).thenReturn(true);
+		
+		hibernateEventInterceptor.afterTransactionCompletion(transaction);
+		
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.CREATED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.DELETED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.UPDATED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.RETIRED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.UNRETIRED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.VOIDED));
+		verify(eventManager, times(1)).serveEvent(eq(openmrsObject), eq(EventAction.UNVOIDED));
+	}
+	
+	@Test
+	public void afterTransactionCompletion_shouldNotServeEventsWhenTransationWasNotCommited() {
+		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
+		
+		addOpenmrsObjectToAllSets();
+		
+		final Transaction transaction = mock(Transaction.class);
+		when(transaction.wasCommitted()).thenReturn(false);
+		
+		hibernateEventInterceptor.afterTransactionCompletion(transaction);
+		
+		verify(eventManager, never()).serveEvent(any(), any());
+	}
+	
+	@Test
+	public void afterTransactionCompletion_shouldCleanupSetsAtTheEndOfMethod() {
+		hibernateEventInterceptor.afterTransactionBegin(null); // init structure
+		
+		addOpenmrsObjectToAllSets();
+		
+		final Transaction transaction = mock(Transaction.class);
+		when(transaction.wasCommitted()).thenReturn(false);
+		
+		hibernateEventInterceptor.afterTransactionCompletion(transaction);
+		
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+		Assert.assertNull(inserts.get());
+	}
+	
+	private void addOpenmrsObjectToAllSets() {
+		inserts.get().peek().add(openmrsObject);
+		deletes.get().peek().add(openmrsObject);
+		updates.get().peek().add(openmrsObject);
+		retiredObjects.get().peek().add(openmrsObject);
+		unretiredObjects.get().peek().add(openmrsObject);
+		voidedObjects.get().peek().add(openmrsObject);
+		unvoidedObjects.get().peek().add(openmrsObject);
 	}
 	
 	private void initVariables() {
