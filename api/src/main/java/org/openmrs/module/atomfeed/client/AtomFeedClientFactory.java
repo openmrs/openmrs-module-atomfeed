1c231f53b025141f64a974b07719126b9454953a
Jakub Trzebiatowski <jtrzebiatowski@soldevelo.com>
2017-11-29 16:44:22 +0100

    ATF-15: Create AtomFeedClientFactory

---
+++ api/src/main/java/org/openmrs/module/atomfeed/client/AtomFeedClientFactory.java
@@ -0,0 +1,27 @@
+package org.openmrs.module.atomfeed.client;
+
+import org.ict4h.atomfeed.client.service.EventWorker;
+import org.openmrs.module.atomfeed.client.impl.AtomFeedClientImpl;
+
+public class AtomFeedClientFactory {
+
+    private final static int SECOND = 1000;
+    private final static int CONNECT_TIMEOUT = 15 * SECOND;
+    private final static int READ_TIMEOUT = 30 * SECOND;
+
+    private AtomFeedClientFactory() { }
+
+    public static AtomFeedClient createClient() {
+        return createClient(null);
+    }
+
+    public static AtomFeedClient createClient(EventWorker eventWorker) {
+        AtomFeedClient atomFeedClient =  new AtomFeedClientImpl(eventWorker);
+
+        atomFeedClient.setConnectTimeout(CONNECT_TIMEOUT);
+        atomFeedClient.setReadTimeout(READ_TIMEOUT);
+
+        return atomFeedClient;
+    }
+
+}
