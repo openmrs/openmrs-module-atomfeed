f32c850abda7d61bab7cb70898699adc7251210f
Karol Cissewski <kcissewski@soldevelo.com>
2018-10-17 14:54:23 +0200

    SYNCT-201: Fix getting Feed Filters from tags

--- api/src/main/java/org/openmrs/module/atomfeed/api/service/impl/TagsServiceImpl.java
+++ api/src/main/java/org/openmrs/module/atomfeed/api/service/impl/TagsServiceImpl.java
@@ -1,16 +1,20 @@
 package org.openmrs.module.atomfeed.api.service.impl;
 
+import com.sun.syndication.feed.atom.Category;
 import org.openmrs.module.atomfeed.api.filter.FeedFilter;
 import org.openmrs.module.atomfeed.api.service.TagService;
 import org.openmrs.module.atomfeed.api.service.XMLParseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.xml.bind.JAXBException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
+@Component("atomfeed.tagsService")
 public class TagsServiceImpl implements TagService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TagService.class);
@@ -22,15 +26,18 @@ public class TagsServiceImpl implements TagService {
 	public List<FeedFilter> getFeedFiltersFromTags(List tags) {
 		List<FeedFilter> feedFilters = new ArrayList<>();
 
-		for (Object tag : tags) {
-			if (tag instanceof String) {
+		ListIterator tagIterator = tags.listIterator();
+		while (tagIterator.hasNext()) {
+			Object tag = tagIterator.next();
+			if (tag instanceof Category) {
 				try {
-					FeedFilter feedFilter = xmlParseService.createFeedFilterFromXMLString((String) tag);
+					FeedFilter feedFilter = xmlParseService.createFeedFilterFromXMLString(((Category) tag).getTerm());
 					if (feedFilter.getBeanName() != null && feedFilter.getFilter() != null) {
 						feedFilters.add(feedFilter);
+						tagIterator.remove(); // Remove processed tag
 					}
 				} catch (JAXBException e) {
-					LOGGER.warn(String.format("Cannot parse tag to XML: %s", (String) tag));
+					LOGGER.warn(String.format("Cannot parse tag to XML: %s", ((Category) tag).getTerm()));
 				}
 			}
 		}
